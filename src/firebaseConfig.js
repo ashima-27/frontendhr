8a120db054ce398da6906835b49de0e853213ae4
duggalashima905@gmail.com <duggalashima905@gmail.com>
2024-06-21 13:04:01 +0530

    hcng

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,13 +5,13 @@ import { getAnalytics } from "firebase/analytics";
 import {getMessaging} from "firebase/messaging";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBygb2BrrEoSLhMlWYR5aadPAry8avkkxw",
-    authDomain: "employee-management-dfd2b.firebaseapp.com",
-    projectId: "employee-management-dfd2b",
-    storageBucket: "employee-management-dfd2b.appspot.com",
-    messagingSenderId: "1063426359440",
-    appId: "1:1063426359440:web:8686d8ba093e2f7850d815",
-    measurementId: "G-68402FTLXL"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
